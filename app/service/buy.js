13c157e7333f8863378fa949c6799121e1beb21c
Daniel Husar <dano.husar@gmail.com>
2015-02-18 21:37:05 +0000

    buy timeout

--- app/service/buy.js
+++ app/service/buy.js
@@ -5,7 +5,8 @@ var gui = global.window.nwDispatcher.requireNwGui();
 var win = gui.Window.get();
 var $ = global.window.jQuery;
 var items = [];
-var buy = '$J.ajax({ \
+var buy = 'window.buy = window.buy || {}; \
+					 $J.ajax({ \
 							url:"https://steamcommunity.com/market/buylisting/{0}", \
 							type:"POST", \
 							data: { \
@@ -20,7 +21,20 @@ var buy = '$J.ajax({ \
 						xhrFields: { \
 							withCredentials: true \
 						} \
-					});';
+					}) \
+					.done(function (data) { \
+						window.buy[{0}] = { \
+							success: data \
+						} \
+					}) \
+				  .fail(function (err, text, jqXHR) { \
+				  	window.buy[{0}] = { \
+							err: err, \
+							text: text, \
+							jqXHR: jqXHR \
+						} \
+				  });';
+var iframe = $('#ifr')[0];
 
 module.exports = function (app) {
 	app.service('BuyService', function (UserDetailsService, FilterItemsFactory) {
@@ -30,12 +44,15 @@ module.exports = function (app) {
 				items.push(item);
 				FilterItemsFactory.set(item.id);
 				var script = fmt(buy, item.id, item.priceFee * 100, item.priceNoFee * 100, ((item.priceFee - item.priceNoFee) * 100));
-				console.log(script);
 
 				win.eval(
-					$('#ifr')[0],
+					iframe,
 					script
 				);
+
+				setInterval(function () {
+					console.log(iframe.contentWindow.buy);
+				}, 1000);
 
 			},
 
